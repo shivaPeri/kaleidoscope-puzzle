b84c2078eb6e3f3ac318195bba4c34055ccb73bc
shivaPeri <shivaperi47@gmail.com>
2023-01-29 21:28:12 -0500

    added webscraping boilerplate + gitingore

--- python/parseBoard.py
+++ python/parseBoard.py
@@ -1,6 +1,15 @@
 import cv2 as cv
 import numpy as np
 
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+url = "http://www.users.on.net/~mikegatley/kaleidoscope/all1.html"
+url2 = "http://www.users.on.net/~mikegatley/kaleidoscope/all2.html"
+
 colors = np.array([[0,0,0],         # black
                     [255,0,0],      # red
                     [255,255,0],    # yellow
@@ -23,6 +32,31 @@ def im2board(im):
             out += str(scores.argmin())
 
     return out
+
+def scrapeBoardsFromURL(url):
+    with webdriver.Chrome(executable_path='/Users/shivaperi/bin/chromedriver') as driver:
+
+        driver.get(url)
+        time.sleep(30)
+
+        i = 0
+        while i < 200:
+
+            wait = WebDriverWait(driver, 2)
+
+            try:
+                button = wait.until(presence_of_element_located((By.ID, 'download-button-' + model_id)))
+                button.click()
+
+                button = wait.until(presence_of_element_located((By.ID, 'download-option-zip')))
+                button.click()
+            except:
+                continue
+            
+            print(i)
+            i += 1
+
+    print('done scraping')
 
 if __name__ == "__main__":
 
