fcc08120b306843ad4fabcb5c513dd06c1995f05
shivaPeri <shivaperi47@gmail.com>
2023-01-31 16:36:31 -0500

    fixed spacing

--- src/game.rs
+++ src/game.rs
@@ -64,50 +64,50 @@ fn load_pieces_2() -> Vec<Piece2> {
     let domo_1 = Piece2{
         idx: 3,
         configs: vec![
-            vec![1,2, RED, BLACK],
-            vec![1,2, BLACK, RED],
-            vec![1,2, BLUE, BLACK],
-            vec![1,2, BLACK, BLUE],
-            vec![2,1, RED, BLACK],
-            vec![2,1, BLACK, RED],
-            vec![2,1, BLUE, BLACK],
-            vec![2,1, BLACK, BLUE],
+            vec![1,2, RED,BLACK],
+            vec![1,2, BLACK,RED],
+            vec![1,2, BLUE,BLACK],
+            vec![1,2, BLACK,BLUE],
+            vec![2,1, RED,BLACK],
+            vec![2,1, BLACK,RED],
+            vec![2,1, BLUE,BLACK],
+            vec![2,1, BLACK,BLUE],
         ];
     };
 
     let trom_1 = Piece2{
         idx: 4,
         configs: vec![
-            vec![1,3, RED, BLACK, BLACK],
-            vec![1,3, YELLOW, BLACK, BLUE],
-            vec![1,3, BLUE, BLACK, YELLOW],
-            vec![3,1, RED, BLACK, BLACK],
-            vec![3,1, YELLOW, BLACK, BLUE],
-            vec![3,1, BLUE, BLACK, YELLOW],
+            vec![1,3, RED,BLACK,BLACK],
+            vec![1,3, YELLOW,BLACK,BLUE],
+            vec![1,3, BLUE,BLACK,YELLOW],
+            vec![3,1, RED,BLACK,BLACK],
+            vec![3,1, YELLOW,BLACK,BLUE],
+            vec![3,1, BLUE,BLACK,YELLOW],
         ];
     };
 
     let trom_2 = Piece2{
         idx: 5,
         configs: vec![
-            vec![1,3, BLACK, RED, BLACK],
-            vec![1,3, BLACK, YELLOW, BLACK],
-            vec![3,1, BLACK, RED, BLACK],
-            vec![3,1, BLACK, YELLOW, BLACK],
+            vec![1,3, BLACK,RED,BLACK],
+            vec![1,3, BLACK,YELLOW,BLACK],
+            vec![3,1, BLACK,RED,BLACK],
+            vec![3,1, BLACK,YELLOW,BLACK],
         ];
     };
 
     let trom_3 = Piece2{
         idx: 6,
         configs: vec![
-            vec![2,2, EMPTY, BLACK, RED, BLACK],
-            vec![2,2, BLACK, EMPTY, RED, BLACK],
-            vec![2,2, BLACK, RED, EMPTY, BLACK],
-            vec![2,2, BLACK, RED, BLACK, EMPTY],
-            vec![2,2, EMPTY, BLACK, YELLOW, BLACK],
-            vec![2,2, BLACK, EMPTY, YELLOW, BLACK],
-            vec![2,2, BLACK, YELLOW, EMPTY, BLACK],
-            vec![2,2, BLACK, YELLOW, BLACK, EMPTY],
+            vec![2,2, EMPTY,BLACK,RED,BLACK],
+            vec![2,2, BLACK,EMPTY,RED,BLACK],
+            vec![2,2, BLACK,RED,EMPTY,BLACK],
+            vec![2,2, BLACK,RED,BLACK,EMPTY],
+            vec![2,2, EMPTY,BLACK,YELLOW,BLACK],
+            vec![2,2, BLACK,EMPTY,YELLOW,BLACK],
+            vec![2,2, BLACK,YELLOW,EMPTY,BLACK],
+            vec![2,2, BLACK,YELLOW,BLACK,EMPTY],
         ];
     };
 
@@ -115,78 +115,78 @@ fn load_pieces_2() -> Vec<Piece2> {
     let trom_4 = Piece2{
         idx: 7,
         configs: vec![
-            vec![2,2, EMPTY, RED, BLACK, RED],
-            vec![2,2, RED, EMPTY, BLACK, RED],
-            vec![2,2, RED, BLACK, EMPTY, RED],
-            vec![2,2, RED, BLACK, RED, EMPTY],
-            vec![2,2, EMPTY, BLUE, YELLOW, BLACK],
-            vec![2,2, BLUE, EMPTY, YELLOW, BLACK],
-            vec![2,2, BLUE, YELLOW, EMPTY, BLACK],
-            vec![2,2, BLUE, YELLOW, BLACK, EMPTY],
+            vec![2,2, EMPTY,RED,BLACK,RED],
+            vec![2,2, RED,EMPTY,BLACK,RED],
+            vec![2,2, RED,BLACK,EMPTY,RED],
+            vec![2,2, RED,BLACK,RED,EMPTY],
+            vec![2,2, EMPTY,BLUE,YELLOW,BLACK],
+            vec![2,2, BLUE,EMPTY,YELLOW,BLACK],
+            vec![2,2, BLUE,YELLOW,EMPTY,BLACK],
+            vec![2,2, BLUE,YELLOW,BLACK,EMPTY],
         ];
     };
 
     let tetr_1 = Piece2{
         idx: 8,
         configs: vec![
-            vec![1,4, RED, BLACK, RED, BLACK],
-            vec![1,4, BLACK, RED, BLACK, RED],
-            vec![1,4, BLUE, BLACK, YELLOW, BLACK],
-            vec![1,4, BLACK, YELLOW, BLACK, BLUE],
+            vec![1,4, RED,BLACK,RED,BLACK],
+            vec![1,4, BLACK,RED,BLACK,RED],
+            vec![1,4, BLUE,BLACK,YELLOW,BLACK],
+            vec![1,4, BLACK,YELLOW,BLACK,BLUE],
         ];
     };
 
     let tetr_2 = Piece2{
         idx: 9,
         configs: vec![
-            vec![2,2, RED, BLACK, RED, BLACK],
-            vec![2,2, BLACK, RED, BLACK, RED],
-            vec![2,2, BLUE, BLACK, BLACK, YELLOW],
-            vec![2,2, YELLOW, BLACK, BLACK, BLUE],
-            vec![2,2, BLACK, YELLOW, BLUE, BLACK],
-            vec![2,2, BLACK, BLUE, YELLOW, BLACK],
+            vec![2,2, RED,BLACK,RED,BLACK],
+            vec![2,2, BLACK,RED,BLACK,RED],
+            vec![2,2, BLUE,BLACK,BLACK,YELLOW],
+            vec![2,2, YELLOW,BLACK,BLACK,BLUE],
+            vec![2,2, BLACK,YELLOW,BLUE,BLACK],
+            vec![2,2, BLACK,BLUE,YELLOW,BLACK],
         ];
     };
 
     let tetr_3 = Piece2{
         idx: 10,
         configs: vec![
-            vec![2,3, EMPTY, EMPTY, RED, BLACK, RED, BLACK],
-            vec![3,2, RED, BLACK, EMPTY, RED, EMPTY, BLACK],
-            vec![2,3, BLACK, RED, BLACK, RED, EMPTY, EMPTY],
-            vec![3,2, BLACK, EMPTY, RED, EMPTY, BLACK, RED],
-            vec![2,3, BLACK, EMPTY, EMPTY, YELLOW, BLACK, BLUE],
-            vec![3,2, YELLOW, BLACK, BLACK, EMPTY, BLUE, EMPTY],
-            vec![2,3, BLUE, BLACK, YELLOW, EMPTY, EMPTY, BLACK],
-            vec![3,2, EMPTY, BLUE, EMPTY, BLACK, BLACK, YELLOW],
+            vec![2,3, EMPTY,EMPTY,RED,BLACK,RED,BLACK],
+            vec![3,2, RED,BLACK,EMPTY,RED,EMPTY,BLACK],
+            vec![2,3, BLACK,RED,BLACK,RED,EMPTY,EMPTY],
+            vec![3,2, BLACK,EMPTY,RED,EMPTY,BLACK,RED],
+            vec![2,3, BLACK,EMPTY,EMPTY,YELLOW,BLACK,BLUE],
+            vec![3,2, YELLOW,BLACK,BLACK,EMPTY,BLUE,EMPTY],
+            vec![2,3, BLUE,BLACK,YELLOW,EMPTY,EMPTY,BLACK],
+            vec![3,2, EMPTY,BLUE,EMPTY,BLACK,BLACK,YELLOW],
         ];
     };
 
     let tetr_4 = Piece2{
         idx: 11,
         configs: vec![
-            vec![2,3, RED, EMPTY, EMPTY, BLACK, RED, BLACK],
-            vec![3,2, BLACK, RED, RED, EMPTY, BLACK, EMPTY],
-            vec![2,3, BLACK, RED, BLACK, EMPTY, EMPTY, RED],
-            vec![3,2, EMPTY, BLACK, EMPTY, RED, RED, BLACK],
-            vec![2,3, EMPTY, EMPTY, YELLOW, BLACK, BLUE, BLACK],
-            vec![3,2, YELLOW, BLACK, EMPTY, BLUE, EMPTY, BLACK],
-            vec![2,3, BLACK, BLUE, BLACK, YELLOW, EMPTY, EMPTY],
-            vec![3,2, BLACK, EMPTY, BLUE, EMPTY, BLACK, YELLOW],
+            vec![2,3, RED,EMPTY,EMPTY,BLACK,RED,BLACK],
+            vec![3,2, BLACK,RED,RED,EMPTY,BLACK,EMPTY],
+            vec![2,3, BLACK,RED,BLACK,EMPTY,EMPTY,RED],
+            vec![3,2, EMPTY,BLACK,EMPTY,RED,RED,BLACK],
+            vec![2,3, EMPTY,EMPTY,YELLOW,BLACK,BLUE,BLACK],
+            vec![3,2, YELLOW,BLACK,EMPTY,BLUE,EMPTY,BLACK],
+            vec![2,3, BLACK,BLUE,BLACK,YELLOW,EMPTY,EMPTY],
+            vec![3,2, BLACK,EMPTY,BLUE,EMPTY,BLACK,YELLOW],
         ];
     };
 
     let tetr_5 = Piece2{
         idx: 12,
         configs: vec![
-            vec![2,3, EMPTY, EMPTY, BLACK, RED, BLACK, RED],
-            vec![3,2, BLACK, RED, EMPTY, BLACK, EMPTY, RED],
-            vec![2,3, RED, BLACK, RED, BLACK, EMPTY, EMPTY],
-            vec![3,2, RED, EMPTY, BLACK, EMPTY, RED, BLACK],
-            vec![2,3, BLACK, EMPTY, EMPTY, BLUE, BLACK, YELLOW],
-            vec![3,2, BLUE, BLACK, BLACK, EMPTY, YELLOW, EMPTY],
-            vec![2,3, YELLOW, BLACK, BLUE, EMPTY, EMPTY, BLACK],
-            vec![3,2, EMPTY, YELLOW, EMPTY, BLACK, BLACK, BLUE],
+            vec![2,3, EMPTY,EMPTY,BLACK,RED,BLACK,RED],
+            vec![3,2, BLACK,RED,EMPTY,BLACK,EMPTY,RED],
+            vec![2,3, RED,BLACK,RED,BLACK,EMPTY,EMPTY],
+            vec![3,2, RED,EMPTY,BLACK,EMPTY,RED,BLACK],
+            vec![2,3, BLACK,EMPTY,EMPTY,BLUE,BLACK,YELLOW],
+            vec![3,2, BLUE,BLACK,BLACK,EMPTY,YELLOW,EMPTY],
+            vec![2,3, YELLOW,BLACK,BLUE,EMPTY,EMPTY,BLACK],
+            vec![3,2, EMPTY,YELLOW,EMPTY,BLACK,BLACK,BLUE],
         ];
     };
 
@@ -194,50 +194,50 @@ fn load_pieces_2() -> Vec<Piece2> {
     let tetr_6 = Piece2{
         idx: 13,
         configs: vec![
-            vec![2,3, BLACK, EMPTY, EMPTY, RED, BLACK, RED],
-            vec![3,2, RED, BLACK, BLACK, EMPTY, RED, EMPTY],
-            vec![2,3, RED, BLACK, RED, EMPTY, EMPTY, BLACK],
-            vec![3,2, EMPTY, RED, EMPTY, BLACK, BLACK, RED],
-            vec![2,3, EMPTY, EMPTY, BLUE, BLACK, YELLOW, BLACK],
-            vec![3,2, BLUE, BLACK, EMPTY, YELLOW, EMPTY, BLACK],
-            vec![2,3, BLACK, YELLOW, BLACK, BLUE, EMPTY, EMPTY],
-            vec![3,2, BLACK, EMPTY, YELLOW, EMPTY, BLACK, BLUE],
+            vec![2,3, BLACK,EMPTY,EMPTY,RED,BLACK,RED],
+            vec![3,2, RED,BLACK,BLACK,EMPTY,RED,EMPTY],
+            vec![2,3, RED,BLACK,RED,EMPTY,EMPTY,BLACK],
+            vec![3,2, EMPTY,RED,EMPTY,BLACK,BLACK,RED],
+            vec![2,3, EMPTY,EMPTY,BLUE,BLACK,YELLOW,BLACK],
+            vec![3,2, BLUE,BLACK,EMPTY,YELLOW,EMPTY,BLACK],
+            vec![2,3, BLACK,YELLOW,BLACK,BLUE,EMPTY,EMPTY],
+            vec![3,2, BLACK,EMPTY,YELLOW,EMPTY,BLACK,BLUE],
         ];
     };
 
     let tetr_7 = Piece2{
         idx: 14,
         configs: vec![
-            vec![2,3, EMPTY, BLACK, EMPTY, BLACK, RED, BLACK],
-            vec![3,2, BLACK, EMPTY, RED, BLACK, BLACK, EMPTY],
-            vec![2,3, BLACK, RED, BLACK, EMPTY, BLACK, EMPTY],
-            vec![3,2, EMPTY, BLACK, BLACK, RED, EMPTY, BLACK],
-            vec![2,3, EMPTY, YELLOW, EMPTY, BLUE, BLACK, YELLOW],
-            vec![3,2, BLUE, EMPTY, BLACK, YELLOW, YELLOW, EMPTY],
-            vec![2,3, YELLOW, BLACK, BLUE, EMPTY, YELLOW, EMPTY],
-            vec![3,2, EMPTY, YELLOW, YELLOW, BLACK, EMPTY, BLUE],
+            vec![2,3, EMPTY,BLACK,EMPTY,BLACK,RED,BLACK],
+            vec![3,2, BLACK,EMPTY,RED,BLACK,BLACK,EMPTY],
+            vec![2,3, BLACK,RED,BLACK,EMPTY,BLACK,EMPTY],
+            vec![3,2, EMPTY,BLACK,BLACK,RED,EMPTY,BLACK],
+            vec![2,3, EMPTY,YELLOW,EMPTY,BLUE,BLACK,YELLOW],
+            vec![3,2, BLUE,EMPTY,BLACK,YELLOW,YELLOW,EMPTY],
+            vec![2,3, YELLOW,BLACK,BLUE,EMPTY,YELLOW,EMPTY],
+            vec![3,2, EMPTY,YELLOW,YELLOW,BLACK,EMPTY,BLUE],
         ];
     };
 
     let tetr_8 = Piece2{
         idx: 15,
         configs: vec![
-            vec![2,3, EMPTY, RED, EMPTY, RED, BLACK, RED],
-            vec![3,2, RED, EMPTY, BLACK, RED, RED, EMPTY],
-            vec![2,3, RED, BLACK, RED, EMPTY, RED, EMPTY],
-            vec![3,2, EMPTY, RED, RED, BLACK, EMPTY, RED],
-            vec![2,3, EMPTY, BLACK, EMPTY, BLACK, BLUE, BLACK],
-            vec![3,2, BLACK, EMPTY, BLUE, BLACK, BLACK, EMPTY],
-            vec![2,3, BLACK, BLUE, BLACK, EMPTY, BLACK, EMPTY],
-            vec![3,2, EMPTY, BLACK, BLACK, BLUE, EMPTY, BLACK],
+            vec![2,3, EMPTY,RED,EMPTY,RED,BLACK,RED],
+            vec![3,2, RED,EMPTY,BLACK,RED,RED,EMPTY],
+            vec![2,3, RED,BLACK,RED,EMPTY,RED,EMPTY],
+            vec![3,2, EMPTY,RED,RED,BLACK,EMPTY,RED],
+            vec![2,3, EMPTY,BLACK,EMPTY,BLACK,BLUE,BLACK],
+            vec![3,2, BLACK,EMPTY,BLUE,BLACK,BLACK,EMPTY],
+            vec![2,3, BLACK,BLUE,BLACK,EMPTY,BLACK,EMPTY],
+            vec![3,2, EMPTY,BLACK,BLACK,BLUE,EMPTY,BLACK],
         ];
     };
 
     let tetr_9 = Piece2{
         idx: 16,
         configs: vec![
-            vec![2,3, EMPTY, BLACK, RED, BLACK, RED, EMPTY],
-            vec![3,2, RED, EMPTY, BLACK, RED,EMPTY,BLACK],
+            vec![2,3, EMPTY,BLACK,RED,BLACK,RED,EMPTY],
+            vec![3,2, RED,EMPTY,BLACK,RED,EMPTY,BLACK],
             vec![2,3, EMPTY,RED,BLACK,RED,BLACK,EMPTY],
             vec![3,2, BLACK,EMPTY,RED,BLACK,EMPTY,RED],
             vec![2,3, BLACK,BLUE,EMPTY,EMPTY,BLACK,YELLOW],
@@ -264,14 +264,14 @@ fn load_pieces_2() -> Vec<Piece2> {
     let oct_1 = Piece2{
         idx: 18,
         configs: vec![
-            vec![1,8, BLACK, RED, BLACK, RED, BLACK, RED, BLACK, RED],
-            vec![1,8, RED, BLACK, RED, BLACK, RED, BLACK, RED, BLACK],
-            vec![8,1, BLACK, RED, BLACK, RED, BLACK, RED, BLACK, RED],
-            vec![8,1, RED, BLACK, RED, BLACK, RED, BLACK, RED, BLACK],
-            vec![1,8, BLACK, YELLOW, BLACK, BLUE, BLACK, YELLOW, BLACK, BLUE],
-            vec![1,8, BLUE, BLACK, YELLOW, BLACK, BLUE, BLACK, YELLOW, BLACK],
-            vec![8,1, BLACK, YELLOW, BLACK, BLUE, BLACK, YELLOW, BLACK, BLUE],
-            vec![8,1, BLUE, BLACK, YELLOW, BLACK, BLUE, BLACK, YELLOW, BLACK],
+            vec![1,8, BLACK,RED,BLACK,RED,BLACK,RED,BLACK,RED],
+            vec![1,8, RED,BLACK,RED,BLACK,RED,BLACK,RED,BLACK],
+            vec![8,1, BLACK,RED,BLACK,RED,BLACK,RED,BLACK,RED],
+            vec![8,1, RED,BLACK,RED,BLACK,RED,BLACK,RED,BLACK],
+            vec![1,8, BLACK,YELLOW,BLACK,BLUE,BLACK,YELLOW,BLACK,BLUE],
+            vec![1,8, BLUE,BLACK,YELLOW,BLACK,BLUE,BLACK,YELLOW,BLACK],
+            vec![8,1, BLACK,YELLOW,BLACK,BLUE,BLACK,YELLOW,BLACK,BLUE],
+            vec![8,1, BLUE,BLACK,YELLOW,BLACK,BLUE,BLACK,YELLOW,BLACK],
         ];
     };
 
