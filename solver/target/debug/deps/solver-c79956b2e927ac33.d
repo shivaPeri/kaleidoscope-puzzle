985a542ff2cd142e4b3dabe2310c151c3d2b637a
shivaPeri <shivaperi47@gmail.com>
2023-01-31 07:54:24 -0500

    restructured project

--- solver/target/debug/deps/solver-c79956b2e927ac33.d
+++
@@ -1,5 +0,0 @@
-/Users/shivaperi/Documents/SideProjects/kaleidoscope/kaleidoscope-puzzle/solver/target/debug/deps/solver-c79956b2e927ac33.rmeta: src/main.rs
-
-/Users/shivaperi/Documents/SideProjects/kaleidoscope/kaleidoscope-puzzle/solver/target/debug/deps/solver-c79956b2e927ac33.d: src/main.rs
-
-src/main.rs:
