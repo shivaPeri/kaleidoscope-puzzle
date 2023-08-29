2c72fa3cd15d296cbea6b40ed13092eac6c3dca4
shivaPeri <shivaperi47@gmail.com>
2023-08-29 07:54:27 -0400

    removed old Board class; moved Board2() -> Board()

--- frontend/js/render.js
+++ frontend/js/render.js
@@ -31,11 +31,12 @@ function setup() {
   selector = createSelect();
   selector.size(selector_size);
   selector.position((width - selector_size) / 2, 70)
-  selector.changed(parseBoard);
+  selector.changed(() => {
+    board.init()
+    for (var piece of gamepieces) piece.respawn()
+  });
 
   let start_x = (width - (8 * SQSIZE)) / 2
-  // refboard = new Board(puzzles["new"], start_x, 100, true)
-  // gameboard = new Board(puzzles["new2"], start_x, 100, false)
 
   fetch('../../boards/scraped-boards.json')
     .then(response => response.json())
@@ -46,10 +47,9 @@ function setup() {
         selector.option(key);
       }
       selector.selected(game_name)
-      board = new Board2(start_x, 100)
+      board = new Board(start_x, 100)
       console.log(board)
 
-      // parseBoard()
       draw()
     })
     .catch(error => console.log(error))
@@ -233,152 +233,22 @@ class Piece {
 }
 
 class Board {
-  constructor(ndarray, x, y, isRef) {
-    this.arr = ndarray
-    this.notes = notes
-    this.x = x
-    this.y = y
-    this.ref = isRef
-
-    this.w = ndarray[0].length * SQSIZE
-    this.h = ndarray.length * SQSIZE
-  }
-
-  // drawNotes() {
-  //   for (let i = 0; i < this.arr.length; i++) {
-  //     for (let j = 0; j < this.arr[0].length; j++) {
-
-  //       let offset = 3
-  //       let sx = this.x + i * SQSIZE
-  //       let sy = this.y + j * SQSIZE
-  //       stroke(255)
-  //       strokeWeight(5)
-  //       strokeCap(ROUND)
-  //       line(sx + offset, sy, sx + SQSIZE - offset, sy)
-  //       line(sx, sy + offset, sx, sy + SQSIZE - offset)
-  //     }
-  //   }
-  // }
-
-  draw() {
-    noStroke()
-    for (var i = 0; i < this.arr.length; i++) {
-      for (var j = 0; j < this.arr[0].length; j++) {
-
-        switch (this.arr[i][j]) {
-          case RED: this.ref ? fill(REDCLEAR) : fill(REDFILL); break;
-          case BLACK: this.ref ? fill(BLACKCLEAR) : fill(BLACKFILL); break;
-          case BLUE: this.ref ? fill(BLUECLEAR) : fill(BLUEFILL); break;
-          case YELLOW: this.ref ? fill(YELLOWCLEAR) : fill(YELLOWFILL); break;
-          default: fill(255, 10)
-          // default: stroke(100); strokeWeight(2); noFill();
-        }
-
-        rect(this.x + j * SQSIZE, this.y + i * SQSIZE, SQSIZE, SQSIZE)
-      }
-    }
-  }
-
-  mouseOver() {
-    return (mouseX > this.x && mouseX < this.x + this.w &&
-      mouseY > this.y && mouseY < this.y + this.h)
-  }
-
-  // snap piece location to closest legal board placement
-  // returns boolean value on sucess or failure
-  place() {
-
-    if (selected_piece != null) {
-      let piece = gamepieces[selected_piece]
-
-      let dx = piece.arr.shape[1] * SQSIZE / 2
-      let dy = piece.arr.shape[0] * SQSIZE / 2
-
-      // x and y of first cell in piece
-      let x = piece.x - dx + SQSIZE / 2
-      let y = piece.y - dy + SQSIZE / 2
-
-      // find min distance cell
-      let min_d = SQSIZE
-      let min_x = gameboard.x
-      let min_y = gameboard.y
-      let min_i = 0
-      let min_j = 0
-
-      for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-          let bx = gameboard.x + SQSIZE * i
-          let by = gameboard.y + SQSIZE * j
-
-          if (dist(x, y, bx, by) < min_d) {
-            min_d = dist(x, y, bx, by)
-            min_x = bx
-            min_y = by
-            min_i = i
-            min_j = j
-          }
-        }
-      }
-
-      // bounds check
-      let [w, h] = piece.dims()
-      let piece_fits = true
-      if (min_i + w <= 8 || min_h + h <= 8) {
-        // check all cells are empty
-        for (let i = min_i; i < min_i + w; i++) {
-          for (let j = min_j; j < min_j + h; j++) {
-            if (gameboard.arr[i][j] != -1) {
-              piece_fits = false
-            }
-          }
-        }
-
-      }
-
-      // actually place piece onto board
-      if (piece_fits) {
-        for (let i = min_i; i < min_i + w; i++) {
-          for (let j = min_j; j < min_j + h; j++) {
-            if (piece.arr.get(i - min_i, j - min_j, 0) != 0) {
-              gameboard.arr[i][j] = selected_piece
-            }
-          }
-        }
-
-        piece.placed = true
-        piece.snapTo(createVector(min_x, min_y))
-        selected_piece = null
-        return true
-      }
-
-      // unmark board
-      for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-          if (gameboard.arr[i][j] == selected_piece) {
-            gameboard.arr[i][j] = -1
-          }
-        }
-      }
-      piece.placed = false
-      piece.x = piece.spawn_x
-      piece.y = piece.spawn_y
-    }
-
-    return false
-  }
-}
-
-class Board2 {
   constructor(x, y) {
     this.pos = createVector(x, y)
 
     this.sol = nj.zeros([8, 8]).subtract(1)
     this.ref = nj.zeros([8, 8])
-    this.parseBoard()
+    this.parseBoardString()
     // this.notes = notes
 
     this.width = 8 * SQSIZE
     this.height = 8 * SQSIZE
+  }
+
+  init() {
+    this.sol = nj.zeros([8, 8]).subtract(1)
+    this.ref = nj.zeros([8, 8])
+    this.parseBoardString()
   }
 
   draw() {
@@ -414,7 +284,7 @@ class Board2 {
   }
 
   // looks up selected game -> populates this.ref ndarray
-  parseBoard() {
+  parseBoardString() {
     let str = games[selector.value()]
     for (let i = 0; i < str.length; i++) {
       let row = Math.floor(i / 8);
@@ -528,15 +398,6 @@ const dist = (x1, y1, x2, y2) => {
   return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
 }
 
-const parseBoard = () => {
-  let str = games[selector.value()]
-  for (let i = 0; i < str.length; i++) {
-    let row = Math.floor(i / 8);
-    let col = i % 8;
-    refboard.arr[row][col] = parseInt(str[i])
-  }
-}
-
 /* ***************** EVENT HANDLERS ******************* */
 
 function windowResized() {
@@ -552,9 +413,9 @@ function mouseDragged() {
   }
 }
 
-// function mouseMoved() {
-//   redraw()
-// }
+function mouseMoved() {
+  redraw()
+}
 
 function keyPressed() {
   if (selected_piece != null) {
