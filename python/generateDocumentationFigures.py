68f4ac1c0579b0c499ff4cb5ec0b24251a663ec0
shivaPeri <shivaperi47@gmail.com>
2024-12-15 02:28:50 -0500

    updated test animation

--- python/generateDocumentationFigures.py
+++ python/generateDocumentationFigures.py
@@ -1,41 +1,50 @@
 import json
-import cv2 as cv
 import numpy as np
 from PIL import Image
 
-# Read the JSON file
-with open(input_path, 'r') as fd:
-    data = json.load(fd)
+def load_board_from_file(input_path):
+    with open(input_path, 'r') as fd:
+        data = json.load(fd)
+        return data
 
 
 class ImageFrame():
-    
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.data = np.array(width, height, 4) # 4 channels for rgba
+        
+        # 4 channels for rgba
+        self.data = np.zeros((width, height, 4), dtype=np.int32) 
     
     def _get_cell_color(self, color):
-        if color == "1": return np.array([0, 0, 0, 100])        # black
-        if color == "2": return np.array([218, 56, 50, 100])    # red
-        if color == "3": return np.array([255, 222, 36, 100])   # yellow
-        if color == "4": return np.array([32, 89, 168, 100])    # blue
+        if color == "1": return np.array([0, 0, 0, 255])        # black
+        if color == "2": return np.array([218, 56, 50, 255])    # red
+        if color == "3": return np.array([255, 222, 36, 255])   # yellow
+        if color == "4": return np.array([32, 89, 168, 255])    # blue
         return np.array([0, 0, 0, 0])                           # transparent
     
     def _get_cell_color_2_bit(self, color):
-        if color == "00": return np.array([0, 0, 0, 100])        # black
-        if color == "01": return np.array([218, 56, 50, 100])    # red
-        if color == "10": return np.array([255, 222, 36, 100])   # yellow
-        if color == "11": return np.array([32, 89, 168, 100])    # blue
+        if color == "00": return np.array([0, 0, 0, 255])        # black
+        if color == "01": return np.array([218, 56, 50, 255])    # red
+        if color == "10": return np.array([255, 222, 36, 255])   # yellow
+        if color == "11": return np.array([32, 89, 168, 255])    # blue
         return np.array([0, 0, 0, 0])                            # transparent
     
     def _draw_board_2_bit(self, board, x, y):
-        pass
+        colors = [board[i:i+2] for i in range(0, len(board), 2)]
+        for i in range(8):    
+            for j in range(8):     
+                color_idx = i * 8 + j
+                self.data[x + j][y + i] = self._get_cell_color_2_bit(colors[color_idx])
     
     def _draw_board(self, board, x, y):
-        pass
+        colors = [board[i] for i in range(len(board))]
+        for i in range(8):    
+            for j in range(8):     
+                color_idx = i * 8 + j
+                self.data[x + j][y + i] = self._get_cell_color(colors[color_idx])
     
-    def draw_board(self, board, x, y):
+    def draw_board(self, board, x=0, y=0):
         if len(board) == 64:
             self._draw_board(board, x, y)
         elif len(board) == 64 * 2:
@@ -44,30 +53,36 @@ class ImageFrame():
     def generate_frame(self):
         w, h = self.width, self.height
         img = Image.new("RGBA", (w, h))
-        img.putdata(self.data.reshape(w*h, 4))
+        pixels = self.data.reshape(w*h, 4)
+        pixels = [(item[0], item[1], item[2], item[3]) for item in pixels]
+        img.putdata(pixels)
         return img
+    
+def test():
+    frames = []
+    for i in range(64):
+        img = ImageFrame(20,20)
+        board_string = "1" * i + "0" * (64-i)
+        img.draw_board(board_string)
+        img.draw_board(board_string, 10, 10)
+        frame = img.generate_frame()
+        frames.append(frame)
 
 
-def map2color(i: str):
-    if int(i) == 1: return "0 0 0\n"        # black
-    if int(i) == 2: return "218 56 50\n"    # red
-    if int(i) == 3: return "255 222 36\n"   # yellow
-    if int(i) == 4: return "32 89 168\n"    # blue
-    return ""
+    # # Define custom durations for each frame
+    # durations = [255, 200, 50, 300, 150, 100, 200, 50, 300, 150]  # Milliseconds
 
-for title, board in data.items():
-
-    with open(output_dir + 'tmp.ppm', 'w') as f:
-        f.write("P3\n")
-        f.write("8 8\n")
-        f.write("256\n")
-
-        for c in board:
-            f.write(map2color(c))
-
-    im = Image.open(output_dir + "tmp.ppm")
-    im.convert("RGB").save(output_dir + title + ".png")
+    frames[0].save(
+        "animation.png",
+        save_all=True,
+        append_images=frames[1:],
+        duration=100,
+        # duration=durations,
+        loop=0,
+        format="PNG",
+    )
 
 if __name__ == "__main__":
     print("started")
+    test()
     print("finished")
