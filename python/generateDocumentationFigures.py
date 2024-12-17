abfb84aa1ffb7fcc3e982bf88b6403e5b9bd490c
shivaPeri <shivaperi47@gmail.com>
2024-12-17 09:07:26 -0500

    updated figure generation code

--- python/generateDocumentationFigures.py
+++ python/generateDocumentationFigures.py
@@ -16,13 +16,13 @@ class CanvasFrame():
     
     def _get_cell_color(self, color):
         # 1 bit colors
-        if color == "1": return np.array([0, 0, 0, 255])        # black
+        if color == "1": return np.array([33, 33, 33, 255])        # black
         if color == "2": return np.array([218, 56, 50, 255])    # red
         if color == "3": return np.array([255, 222, 36, 255])   # yellow
         if color == "4": return np.array([32, 89, 168, 255])    # blue
         
         # 2-bit colors
-        if color == "00": return np.array([0, 0, 0, 255])       # black
+        if color == "00": return np.array([33, 33, 33, 255])       # black
         if color == "01": return np.array([218, 56, 50, 255])   # red
         if color == "10": return np.array([255, 222, 36, 255])  # yellow
         if color == "11": return np.array([32, 89, 168, 255])   # blue
@@ -62,6 +62,13 @@ class CanvasFrame():
             for j in range(y, y+h):
                 if self._in_bounds(i,j):
                     self.data[i,j,:] = 0
+                    
+    # change opacity of a given section of the data array 
+    def set_area_opacity(self, x, y, w, h, opacity=50):
+        for i in range(x, x+w):
+            for j in range(y, y+h):
+                if self._in_bounds(i,j):
+                    self.data[i,j,-1] = opacity
     
     def load_board_from_file(board_title="chaos", input_path="../boards/boards.json"):
         with open(input_path, 'r') as fd:
@@ -131,6 +138,8 @@ class PixelAnimation():
             duration=self.durations,
             loop=0,
             format="PNG",
+            optimize=False,
+            compress_level=0          # No compression (fully lossless)
         )
         
         print(f"Saved animation to: {out_path}")
@@ -176,6 +185,7 @@ def generate_figure_1():
     canvas = CanvasFrame(canvas_width, canvas_height)
     
     start_x = 0
+    print(f"{len(piece_placements)} total frames")
     for frame_idx, placement in tqdm(enumerate(piece_placements)):
         mask, board = placement.split(",")
         
@@ -186,9 +196,11 @@ def generate_figure_1():
         canvas.draw_board(mask, 0, start_y, draw_mask=True)
         start_y += board_size + horizontal_spacing
         
+        canvas.set_area_opacity(start_x-vertical_spacing, start_y, 1, 64)
         canvas.draw_bitvector(board, start_x, start_y)
         start_y += bitvector_size + horizontal_spacing
         
+        canvas.set_area_opacity(start_x-vertical_spacing, start_y, 1, 64)
         canvas.draw_bitvector(mask, start_x, start_y, draw_mask=True)
         start_x += vertical_spacing
         
@@ -197,8 +209,45 @@ def generate_figure_1():
         
     animation.export(out_path=f"piece_{piece_idx}_placements.png")
     
+    
+def generate_figure_2(piece_idx=17):
+    # this figure should show the process of pieces are converted to bit strings
+    
+    board_size = 8
+    bitvector_size = board_size * board_size
+    horizontal_spacing = 2
+    
+    piece_placements = get_piece_placements(piece_idx)
+    canvas_height = board_size + horizontal_spacing + board_size
+    canvas_width = board_size
+    
+    animation = PixelAnimation()
+    canvas = CanvasFrame(canvas_width, canvas_height)
+    
+    print(f"{len(piece_placements)} total frames")
+    for frame_idx, placement in tqdm(enumerate(piece_placements)):
+        mask, board = placement.split(",")
+        
+        start_y = 0
+        canvas.draw_board(board, 0, start_y, mask=mask)
+        start_y += board_size + horizontal_spacing
+        
+        canvas.draw_board(mask, 0, start_y, draw_mask=True)
+        start_y += board_size + horizontal_spacing
+        
+        frame = canvas.generate_frame()
+        animation.add_frame(frame)
+        
+    animation.export(out_path=f"piece_{piece_idx}_placements_small.png")
+
+def generate_figure_3():
+    pass
 
 if __name__ == "__main__":
     print("started")
-    generate_figure_1()
-    # test()
+    # generate_figure_1()
+    
+    # for i in range(18):
+    #     generate_figure_2(i)
+    
+    generate_figure_3()
